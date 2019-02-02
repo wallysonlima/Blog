b027d3588d0b81736ea5be2ed9ec23f99f35a135
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-02-02 18:27:23 -0200

    Created AddPostActivity

---
+++ app/src/main/java/wallyson/lima/blog/AddPostActivity.java
@@ -0,0 +1,13 @@
+package wallyson.lima.blog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
