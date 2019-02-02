6c4cc2c716401f296fc0538d5392c07e8c898e01
wallysonlima <wallyson.n.a.lima@gmail.com>
2019-02-02 14:31:21 -0200

    Implemented the authentication user

--- app/src/main/java/wallyson/lima/blog/MainActivity.java
+++ app/src/main/java/wallyson/lima/blog/MainActivity.java
@@ -1,15 +1,34 @@
 package wallyson.lima.blog;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+    private Button loginButton, createButton;
+    private EditText emailField, passwordField;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton = findViewById(R.id.loginButtonEt);
+        createButton = findViewById(R.id.loginCreateAccount);
+        emailField = findViewById(R.id.loginEmailEt);
+        passwordField = findViewById(R.id.loginPasswordEt);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = firebaseAuth.getCurrentUser();
+
+                if ( mUser != null ) {
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Signed In", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( !TextUtils.isEmpty(emailField.getText().toString()) &&
+                        !TextUtils.isEmpty(passwordField.getText().toString())) {
+
+                    String email = emailField.getText().toString();
+                    String pwd = passwordField.getText().toString();
+
+                    login(email, pwd);
+
+                } else {
+
+                }
+            }
+        });
+    }
+
+    private void login(String email, String pwd) {
+        mAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if ( task.isSuccessful() ) {
+                    // Yay !! We're in!
+                    Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_LONG).show();
+                } else {
+                    // No it!
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
